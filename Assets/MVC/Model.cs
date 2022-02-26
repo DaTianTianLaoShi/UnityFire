a9cd1d5fa161d6b40abac2c76c772e081db55f8a
DaTianTianLaoShi <528193894@qq.com>
2022-02-26 20:48:56 +0800

    firstCommit

---
+++ Assets/MVC/Model.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Consot
+public static class Const
+{
+    public static string Event_StartModel="Event_StartModel";
+}
+public abstract class TestEvent:MonoBehaviour
+{
+   //public void implement()
+   //{
+   //  Debug.Log("执行");
+   //}
+   public abstract string Name { get; } 
+   public  virtual void implement()
+   {
+       Debug.Log("基础方法");
+   }
+}
+
+public class stratTest:TestEvent
+{
+    public override string Name
+    {
+        get
+        {
+           //public string a="";
+           return Const.Event_StartModel ;
+        }
+      
+    }
+   // public override string Name => throw new System.NotImplementedException();
+    public override void implement()
+    {
+        Debug.Log("爬爬爬");
+        Debug.Log("开始执行某些属性");
+    }
+}
+
+public class Model : MonoBehaviour
+{
+    public static Dictionary<string, TestEvent> EventDic =  new Dictionary<string, TestEvent>(); 
+    //添加事件
+    public void AddEvent( TestEvent Event )
+    {
+        EventDic.Add(Event.Name,Event);
+    }
+    public void DestroyEvent(TestEvent Event )
+    {
+        EventDic.Remove(Event.Name);
+    }
+    public void SendEvent(string eventName)
+    {
+        if (EventDic.ContainsKey(eventName))
+        {
+            EventDic[eventName].implement();
+        }
+        else
+        {   
+            Debug.Log("错误没有这个事件");     
+        }
+    }
+    void Start()
+    { 
+        AddEvent( new stratTest());
+        SendEvent(Const.Event_StartModel);
+    }
+    //void Update()
+    //{  
+    //}
+}
---
+++ Assets/Scenes/StartFire.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartFire : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         ButtenEvent();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ButtenEvent()
+    {
+        GameObject Text= GameObject.Find("Button");
+        Debug.Log(Text.name);
+        Text.GetComponent<Button>().onClick.AddListener(Fire);
+    }
+    void Fire()
+    {
+        Debug.Log("发送远程事件");
+        TcpClient client =new TcpClient();
+        client.Connect(System.Net.IPAddress.Parse("192.168.0.154"),10015);
+    }
+}
